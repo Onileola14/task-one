d9ea944a99c020d981fe6cb75729c079c4e05f3b
onileola <onileola14@gmail.com>
2026-04-15 17:03:14 +0100

    modified:   controllers/profile.js 	modified:   routes/profileRoute.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,4 @@
+const { StatusCodes } = require('http-status-codes');
 const Profile = require("../models/Profile");
 const fetchApis = require("../utils/fetchApi");
 const classifyAge = require("../utils/classifyAge");
@@ -8,14 +9,14 @@ const createProfile = async (req, res) => {
 
   //  Validation
   if (name === undefined || name === "") {
-    return res.status(400).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
       status: "error",
       message: "Name is required",
     });
   }
 
   if (typeof name !== "string") {
-    return res.status(422).json({
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
       status: "error",
       message: "Name must be a string",
     });
@@ -26,7 +27,7 @@ const createProfile = async (req, res) => {
   // Idempotency check
   const existing = await Profile.findOne({ name: lowerName });
   if (existing) {
-    return res.status(200).json({
+    return res.status(StatusCodes.OK).json({
       status: "success",
       message: "Profile already exists",
       data: existing,
@@ -38,21 +39,21 @@ const createProfile = async (req, res) => {
 
   // 🔹 Edge Cases
   if (!gender.gender || gender.count === 0) {
-    return res.status(404).json({
+    return res.status(StatusCodes.BAD_GATEWAY).json({
       status: "error",
       message: "Gender data unavailable",
     });
   }
 
   if (age.age === null) {
-    return res.status(404).json({
+    return res.status(StatusCodes.BAD_GATEWAY).json({
       status: "error",
       message: "Age data unavailable",
     });
   }
 
   if (!country.country || country.country.length === 0) {
-    return res.status(404).json({
+    return res.status(StatusCodes.BAD_GATEWAY).json({
       status: "error",
       message: "Country data unavailable",
     });
@@ -80,10 +81,72 @@ const createProfile = async (req, res) => {
   // 🔹 Save
   const profile = await Profile.create(profileData);
 
-  return res.status(200).json({
+  return res.status(StatusCodes.CREATED).json({
     status: "success",
     data: profile,
   });
 };
 
-module.exports = { createProfile };
+
+const getSingleProfile = async (req, res) => {
+  const { id } = req.params;
+  const profile = await Profile.findOne({ id });
+
+  if (!profile) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      status: "error",
+      message: "Profile not found",
+    });
+  }
+
+  return res.status(StatusCodes.OK).json({
+    status: "success",
+    data: profile,
+  });
+};
+
+const getAllProfiles = async (req, res) => {
+  const { gender, country_id, age_group } = req.query;
+
+  const queryObject = {};
+
+  // Case-insensitive filtering
+  if (gender) {
+    queryObject.gender = new RegExp(`^${gender}$`, "i");
+  }
+
+  if (country_id) {
+    queryObject.country_id = new RegExp(`^${country_id}$`, "i");
+  }
+
+  if (age_group) {
+    queryObject.age_group = new RegExp(`^${age_group}$`, "i");
+  }
+
+  const profiles = await Profile.find(queryObject).select(
+    "id name gender age age_group country_id"
+  );
+
+  return res.status(StatusCodes.OK).json({
+    status: "success",
+    count: profiles.length,
+    data: profiles,
+  });
+};
+const deleteProfile = async (req, res) => {
+  const { id } = req.params;
+
+  const profile = await Profile.findOneAndDelete({ id });
+
+  if (!profile) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      status: "error",
+      message: "Profile not found",
+    });
+  }
+
+  return res.status(StatusCodes.NO_CONTENT).send();
+};
+
+
+module.exports = { createProfile, getSingleProfile, getAllProfiles, deleteProfile };
--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { createProfile } = require("../controllers/profile");
+const { createProfile,getSingleProfile, getAllProfiles, deleteProfile  } = require("../controllers/profile");
 const router = express.Router();
 
-router.route("/").post(createProfile);
+router.route("/").post(createProfile).get(getAllProfiles);
+router.route("/:id").get(getSingleProfile).delete(deleteProfile);
 
 module.exports = router
