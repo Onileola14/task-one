1ebfa18ae72a8d72429e6c8f4d235229663d85b7
onileola <onileola14@gmail.com>
2026-04-17 03:09:52 +0100

    new file:   app.js 	deleted:    vercel.json

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+require("express-async-error");
+const connectDB = require("./db/connectDB");
+const errorHandlerMiddleware = require("./middlewares/errorHandlerMiddleware");
+const notFoundError = require("./middlewares/not-found-error");
+const profileRoute = require("./routes/profileRoute");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+app.use("/api/profiles", profileRoute);
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundError);
+
+const port = 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`server listening on port ${port}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
